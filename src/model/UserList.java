e0c11831fcfd5eef3cc1683b4b26b0264af57a5c
jtaylor125 <jtaylor125@arizona.edu>
2025-03-05 17:50:22 -0700

    more User, UserList

--- src/model/UserList.java
+++ src/model/UserList.java
@@ -43,10 +43,99 @@ public class UserList {
 			}
 		}
 		
+		if (user == null) {
+			return false;
+		}
+		
 		if (user.logIn(username, password)){
 			this.currentUser = user;
 			return true;
 		}
 		return false;		
+	}
+	
+	public boolean userLogOut() {
+		if (this.currentUser != null) {
+			if (this.currentUser.isLoggedIn()) {
+				this.currentUser.logOut();
+				this.currentUser = null;
+				return true;
+			}
+		}
+		return false;
+		
+	}
+	
+	public String searchLibrary(String s, String songOrAlbum, String titleOrArtist) {
+		return this.currentUser.searchLibrary(s, songOrAlbum, titleOrArtist);
+	}
+	
+	public boolean addSong(String songName, String artist, MusicStore musicStore) {
+		return this.currentUser.addSong(songName, artist, musicStore);
+	}
+	
+	public boolean addAlbum(String albumName, String artist, MusicStore musicStore) {
+		return this.currentUser.addAlbum(albumName, artist, musicStore);
+	}
+	
+	public String getSongTitles() {
+		return this.currentUser.getSongTitles();
+	}
+	
+	public String getArtists() {
+		return this.currentUser.getArtists();
+	}
+	
+	public String getAlbums() {
+		return this.currentUser.getAlbums();
+	}
+	
+	public String getPlaylists() {
+		return this.currentUser.getPlaylists();
+	}
+	
+	public String getFavoriteSongs() {
+		return this.currentUser.getFavoriteSongs();
+	}
+	
+	public boolean createPlaylist(String playlistName) {
+		return this.currentUser.createPlaylist(playlistName);
+	}
+	
+	public boolean addToPlaylist(String songName, String artist, String playlistName) {
+		return this.currentUser.addToPlaylist(songName, artist, playlistName);
+	}
+	
+	public boolean removeFromPlaylist(String songName, String artist, String playlistName) {
+		return this.currentUser.removeFromPlaylist(songName, artist, playlistName);
+	}
+	
+	public boolean checkPlaylistExistence(String playlistName) {
+		return this.currentUser.checkPlaylistExistence(playlistName);
+	}
+	
+	public boolean checkSongInPlaylist(String songName, String artist, String playlistName) {
+		return this.currentUser.checkSongInPlaylist(songName, artist, playlistName);
+	}
+	
+	public boolean checkSongInLibrary(String songName, String artist) {
+		return this.currentUser.checkSongInLibrary(songName, artist);
+	}
+	
+	public void markFavorite(String songName, String artist) {
+		this.currentUser.markFavorite(songName, artist);
+	}
+	
+	public void rateSong(String songName, String artist, int rating) {
+		this.currentUser.rateSong(songName, artist, rating);
+	}
+	
+	public boolean isLoggedIn(String username) {
+		for (User u:userList) {
+			if (u.getUsername().equals(username) && u.isLoggedIn()) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- src/model/UserTest.java
+++ src/model/UserTest.java
@@ -118,4 +118,23 @@ public class UserTest {
 		
 		assertFalse(user.isLoggedIn());
 	}
+	
+	@Test
+	void DELETETHIS() {
+		int a = 1;
+		int b = 2;
+		double x = 1.1;
+		double y = 2.2;
+		String s = "hi";
+		String t = "bye";
+		
+		System.out.println(a + b + s);
+		System.out.println(s + a + b);
+		System.out.println(s + (a + b));
+		System.out.println(a + x);
+		System.out.println(x+y);
+		System.out.println(a + s);
+		System.out.println(s + y);
+		System.out.println(s + t);
+	}
 }
